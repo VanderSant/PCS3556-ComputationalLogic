e63425fab3ef9037151f27250a282a32a91d3ad6
VanderSant <vanderson.santos@enline-transmission.com>
2023-02-26 21:13:46 -0300

    🧪 add some unit test

--- ep2/project.clj
+++ ep2/project.clj
@@ -1,3 +1,8 @@
 (defproject PCS3556/ep2 "0.1.0-SNAPSHOT"
   :main ep2.core
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [
+    [org.clojure/clojure "1.5.1"]
+  ]
+
+  :test-paths ["test/"]
+)
---
+++ ep2/test/ep2/core_test.clj
@@ -0,0 +1,72 @@
+(ns ep2.core-test
+  (:require [clojure.test :refer :all]
+            [ep2.core :refer :all]
+    )
+)
+
+(deftest GetGramRuleTest []
+  (let [
+      rules_test [
+        [ "S" ["a", "A", "S"]  ]
+        [ "A" ["a"]            ]
+      ]
+
+      value_result (ep2.core/GetGramRule rules_test 0)
+
+      value_result1 (ep2.core/GetGramRule rules_test 1)
+    ]
+  
+    (testing "Testando a função GetGramRuleTest"
+      (is (= value_result {"simbol" "S", "value" ["a", "A", "S"] }))
+      (is (= (value_result "simbol") "S" ))
+      (is (= (value_result "value" ) ["a", "A", "S"] ))
+      (is (= value_result1 {"simbol" "A", "value" ["a"]} ))
+    )
+  )
+)
+
+
+(deftest GetApplyRuleInElementTest []
+  (let [
+      rules_test0 [
+        [ "S" ["a", "A", "S"]  ]
+        [ "S" ["a"]            ]
+        [ "A" ["b" "a"]        ]
+      ]
+
+      value_result0 (ep2.core/GetApplyRuleInElement rules_test0 "S")
+      value_result1 (ep2.core/GetApplyRuleInElement rules_test0 "A")
+      value_result2 (ep2.core/GetApplyRuleInElement rules_test0 "a")
+    ]
+  
+    (testing "Testando a função GetGramRuleTest"
+      (is (= value_result0 [["a", "A", "S"] ["a"]] ))
+      (is (= value_result1 [["b" "a"]] ))
+      (is (= value_result2 [] ))
+    )
+  )
+)
+
+(deftest GetApplyRulesInChairTest []
+  (let [
+      rules_test0 [
+        [ "S" ["a", "A", "S"]  ]
+        [ "S" ["a"]            ]
+        [ "A" ["b" "a"]        ]
+      ]
+
+      rules_test1 [
+        [ "S" ["a", "A", "S"]  ]
+        [ "S" ["a"]            ]
+      ]
+
+      value_result0 (ep2.core/GetApplyRulesInChair rules_test0 ["S", "A"])
+      value_result1 (ep2.core/GetApplyRulesInChair rules_test1 ["a", "A", "S"])
+    ]
+  
+    (testing "Testando a função GetGramRuleTest"
+      (is (= value_result0 [["a", "A", "S", "A"], ["a", "A"], ["S", "b", "a"]]  ))
+      (is (= value_result1 [["a", "A", "a", "A", "S"], ["a", "A", "a"]]  ))
+    )
+  )
+)
