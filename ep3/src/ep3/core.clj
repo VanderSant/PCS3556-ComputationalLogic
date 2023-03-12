328242ba75270f100aa5bb89c8cb58355d39b488
VanderSant <vanderson.santos@enline-transmission.com>
2023-03-12 16:57:42 -0300

    🔨 add core functions to ep3

--- ep3/src/ep3/core.clj
+++ ep3/src/ep3/core.clj
@@ -3,7 +3,80 @@
       (:gen-class)
       (:require [clojure.set :as set]))
 
+;; Deterministic functions --------------------
+(defn GenDeterministicFiniteAutomatonStates [states actions next_states]
+      (let
+            [
+                  actions->next_state (vec (map zipmap actions next_states) )
+                  state->action (zipmap states actions->next_state)
+            ]
+
+            state->action
+      )
+)
+
+(defn GetDeterministicNextState [matrix state action]
+      (let 
+            [
+                  next_state (get (get matrix state) action)
+            ]
+            next_state
+      )
+)
+
+(defn GetResultState 
+      ([matrix input] (GetResultState matrix input "Q1"))
+      ([matrix input actual_state]
+
+            (let
+                  [
+                        new_action (first input)
+                        next_state (GetDeterministicNextState matrix actual_state new_action)
+                        new_input (rest input)
+                  ]
+                  (if (not (empty? new_input))
+                        (GetResultState matrix new_input next_state)
+                        next_state
+                  )
+            )
+      )
+
+)
+
+(defn SolveDeterministicFiniteAutomaton [states actions next_states accept_states input]
+      (let
+            [
+                  states_deterministic_matrix (GenDeterministicFiniteAutomatonStates states actions next_states)
+                  final_state (GetResultState states_deterministic_matrix input)
+                  result (contains? (set accept_states) final_state)
+            ]
+            result 
+      )
+)
+
 ;; Main function ----------------------
 (defn -main []
+      (def states  ["Q1""Q2""Q3"])
+
+      (def actions [
+                  ["a" "b" "c" "d"]
+                  ["a" "b" "c"]
+                  ["a" "b" "c" "d"]
+            ]
+      )
+
+      (def next_states 
+            [
+                  ["Q1" "Q2" "Q2" "Q3"]
+                  ["Q2" "Q2" "Q3"]
+                  ["Q2" "Q3" "Q3" "Q3"]
+            ]
+      )
+
+      (def accept_states ["Q2""Q3"])
+
+      (def input ["a","b","c"] )
+
+      (println (SolveDeterministicFiniteAutomaton states actions next_states accept_states input) )
 
 )
