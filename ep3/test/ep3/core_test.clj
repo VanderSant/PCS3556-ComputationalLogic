029cd332766062622f9fa57512e328314ba702a2
VanderSant <vanderson.santos@enline-transmission.com>
2023-03-12 21:05:14 -0300

    🧪 add ep3 non deterministic  tests

--- ep3/test/ep3/core_test.clj
+++ ep3/test/ep3/core_test.clj
@@ -4,17 +4,18 @@
     )
 )
 
-(deftest GenDeterministicFiniteAutomatonStatesTest []
+;; Deterministic functions tests --------------------
+(deftest GenDeterministicTransistionsTest []
   (let [
       states0 ["Q1" "Q2" "Q3"]
       actions0 [["a"] ["a"] ["a"]]
       next_state0 [["Q1"] ["Q2"] ["Q2"]]
-      result0 (ep3.core/GenDeterministicFiniteAutomatonStates states0 actions0 next_state0)
+      result0 (ep3.core/GenDeterministicTransistions states0 actions0 next_state0)
 
       states1 ["Q1" "Q2" "Q3"]
       actions1 [["a" "b" "c"] ["a" "c"] ["a" "d"]]
       next_state1 [["Q1" "Q1" "Q2"] ["Q2" "Q3"] ["Q2" "Q1"]]
-      result1 (ep3.core/GenDeterministicFiniteAutomatonStates states1 actions1 next_state1)
+      result1 (ep3.core/GenDeterministicTransistions states1 actions1 next_state1)
     ]
   
     (testing "Testando a função CreateR"
@@ -28,9 +29,9 @@
   (let [
       matrix {"Q3" {"d" "Q3", "c" "Q3", "b" "Q3", "a" "Q2"}, "Q2" {"c" "Q3", "b" "Q2", "a" "Q2"}, "Q1" {"d" "Q3", "c" "Q2", "b" "Q2", "a" "Q1"}}
 
-      result0 (ep3.core/GetDeterministicNextState matrix "Q3" "a")
-      result1 (ep3.core/GetDeterministicNextState matrix "Q3" "b")
-      result2 (ep3.core/GetDeterministicNextState matrix "Q1" "a")
+      result0 (ep3.core/GetNextState matrix "Q3" "a")
+      result1 (ep3.core/GetNextState matrix "Q3" "b")
+      result2 (ep3.core/GetNextState matrix "Q1" "a")
     ]
   
     (testing "Testando a função CreateR"
@@ -41,21 +42,21 @@
   )
 )
 
-(deftest GetResultStateTest []
+(deftest GetDeterministicResultStateTest []
   (let [
       matrix {"Q3" {"d" "Q3", "c" "Q3", "b" "Q3", "a" "Q2"}, "Q2" {"c" "Q3", "b" "Q2", "a" "Q2"}, "Q1" {"d" "Q3", "c" "Q2", "b" "Q2", "a" "Q1"}}
 
       input0 ["a" "b" "c"]
-      result0 (ep3.core/GetResultState matrix input0 )
+      result0 (ep3.core/GetDeterministicResultState matrix input0 )
 
       input1 ["a" "b"]
-      result1 (ep3.core/GetResultState matrix input1 )
+      result1 (ep3.core/GetDeterministicResultState matrix input1 )
 
       input2 ["a" "d"]
-      result2 (ep3.core/GetResultState matrix input2 )
+      result2 (ep3.core/GetDeterministicResultState matrix input2 )
 
       input3 ["a" "a" "a"]
-      result3 (ep3.core/GetResultState matrix input3 )
+      result3 (ep3.core/GetDeterministicResultState matrix input3 )
     ]
   
     (testing "Testando a função CreateR"
@@ -101,6 +102,103 @@
       (is result0)
       (is (not result1) )
       (is result2)
+    )
+  )
+)
+
+;; Non Deterministic functions Tests --------------------
+
+(deftest GenNonDeterministicTransistionsTest []
+  (let [
+      states0 ["Q1" "Q2" "Q3"]
+      actions0 [["a" "a"] ["a"] ["a"]]
+      next_state0 [["Q1" "Q2"] ["Q2"] ["Q2"]]
+      result0 (ep3.core/GenNonDeterministicTransistions states0 actions0 next_state0)
+    ]
+
+    (testing "Testando a função CreateR"
+      (is (= result0 {"Q3" {"a" ["Q2"]}, "Q2" {"a" ["Q2"]}, "Q1" {"a" ["Q1" "Q2"]}} ) )
+    )
+  )
+)
+
+(deftest GetNonDeterministicNextStateTest []
+  (let [
+      matrix {"Q3" {"a" ["Q2"]}, "Q2" {"a" ["Q2"]}, "Q1" {"a" ["Q1" "Q2"]}}
+
+      result0 (ep3.core/GetNextState matrix "Q3" "a")
+      result1 (ep3.core/GetNextState matrix "Q2" "a")
+      result2 (ep3.core/GetNextState matrix "Q1" "a")
+    ]
+  
+    (testing "Testando a função CreateR"
+      (is (= result0 ["Q2"]))
+      (is (= result1 ["Q2"]))
+      (is (= result2 ["Q1" "Q2"]))
+    )
+  )
+)
+
+(deftest GetNonDeterministicResultStateTest []
+  (let [
+      matrix {"Q3" {"a" ["Q2"], "b" ["Q3"]}, "Q2" {"a" ["Q2"], "b" ["Q1"]}, "Q1" {"a" ["Q1" "Q2"], "b" ["Q3"], "d" ["Q3" "Q2"] }}
+
+      input0 ["b" "a" "a"]
+      result0 (ep3.core/GetNonDeterministicResultState matrix input0 )
+
+      input1 ["a" "b"]
+      result1 (ep3.core/GetNonDeterministicResultState matrix input1 )
+
+      input2 ["d" "b"]
+      result2 (ep3.core/GetNonDeterministicResultState matrix input2 )
+
+      input3 ["a" "a" "a"]
+      result3 (ep3.core/GetNonDeterministicResultState matrix input3 )
+    ]
+  
+    (testing "Testando a função CreateR"
+      (is (= result0 ["Q2"]))
+      (is (= result1 ["Q3" "Q1"]))
+      (is (= result2 ["Q3" "Q1"]))
+      (is (= result3 ["Q1" "Q2"]))
+    )
+  )
+)
+
+(deftest SolveNonDeterministicFiniteAutomatonTest []
+  (let [
+      states  ["Q1""Q2""Q3"]
+
+      actions [
+                ["a" "a" "b" "d" "d"]
+                ["a" "b"]
+                ["a" "b"]
+              ]
+
+      next_states [
+                    ["Q1" "Q2" "Q3" "Q3" "Q2"]
+                    ["Q2" "Q1"]
+                    ["Q2" "Q3"]
+                  ]
+
+      accept_states ["Q3"]
+
+      TestFunction (partial ep3.core/SolveNonDeterministicFiniteAutomaton states actions next_states accept_states)
+
+      input0 ["b" "a" "a"]
+      result0 (TestFunction input0)
+
+      input1 ["a" "b"]
+      result1 (TestFunction input1)
+
+      input2 ["a" "a" "a"]
+      result2 (TestFunction input2)
+    ]
+  
+    (testing "Testando a função CreateR"
+      (is (not result0))
+      (is result1 )
+      (is (not result2) )
     )
   )
 )
