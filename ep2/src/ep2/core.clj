609913d950e45718c82d44a68a1ef8a85eacc2a9
VanderSant <vanderson.santos@enline-transmission.com>
2023-03-20 02:09:26 -0300

    🎨 format ep2

--- ep2/src/ep2/core.clj
+++ ep2/src/ep2/core.clj
@@ -1,123 +1,88 @@
 ;; Definition of namespace -------------
 (ns ep2.core
-      (:gen-class)
-      (:require [clojure.set :as set]))
+  (:gen-class)
+  (:require [clojure.set :as set]))
 
 ;; Basic functions --------------------
 
 (defn GetGramRule [rules pos]
-      (let [
-                  simbol (get (get rules pos) 0)
-                  simbol_values (get (get rules pos) 1)
-                  result {"simbol" simbol, "value" simbol_values } 
-            ]
-      result
-      )
-)
+  (let [simbol (get (get rules pos) 0)
+        simbol_values (get (get rules pos) 1)
+        result {"simbol" simbol, "value" simbol_values}]
+    result))
 
 ;; Chain recognition functions --------------------
 
 (defn GetApplyRuleInElement [rules elem]
-      (let [
-                  indexes (range (count rules))
-                  GetRules (partial GetGramRule rules) 
-                  GetNewValue (fn [rule] (if (= (rule "simbol") elem ) (rule "value") [] ) )
-                  GetNewValueWithRule (fn [index] (GetNewValue (GetRules index) ) ) 
-                  GetAllNewValues (remove empty? (vec (map GetNewValueWithRule indexes ) ) )
-            ]
-      GetAllNewValues
-      )
-)
+  (let [indexes (range (count rules))
+        GetRules (partial GetGramRule rules)
+        GetNewValue (fn [rule] (if (= (rule "simbol") elem) (rule "value") []))
+        GetNewValueWithRule (fn [index] (GetNewValue (GetRules index)))
+        GetAllNewValues (remove empty? (vec (map GetNewValueWithRule indexes)))]
+    GetAllNewValues))
 
-(defn GetApplyRulesInChain 
-      ([rules, chain] (GetApplyRulesInChain rules, chain, 0, []))
-      ([rules, chain, index] (GetApplyRulesInChain rules, chain, index, []))
-      ([rules, chain, index, chain_applied]
+(defn GetApplyRulesInChain
+  ([rules, chain] (GetApplyRulesInChain rules, chain, 0, []))
+  ([rules, chain, index] (GetApplyRulesInChain rules, chain, index, []))
+  ([rules, chain, index, chain_applied]
 
-            (if (= index (count chain))
-                  chain_applied
-                  (let  [
-                              curr_elem (chain index)
-                              
-                              tranf_curr_elem (GetApplyRuleInElement rules curr_elem)
+   (if (= index (count chain))
+     chain_applied
+     (let  [curr_elem (chain index)
 
-                              new_transf (vec 
-                                          (map 
-                                                #(vec (concat (subvec chain 0 index) % (subvec chain (inc index))) )
-                                                tranf_curr_elem
-                                          )
-                                    )
-                              new_chain_applied (vec (concat chain_applied new_transf))
-                        ]
-                  (GetApplyRulesInChain rules chain (+ index 1) new_chain_applied)
-                  )
-            )
-      )
-)
+            tranf_curr_elem (GetApplyRuleInElement rules curr_elem)
+
+            new_transf (vec
+                        (map
+                         #(vec (concat (subvec chain 0 index) % (subvec chain (inc index))))
+                         tranf_curr_elem))
+
+            new_chain_applied (vec (concat chain_applied new_transf))]
+       (GetApplyRulesInChain rules chain (+ index 1) new_chain_applied)))))
 
 (defn GenerateAllPossibleChains
-      ([rules, max_size] (GenerateAllPossibleChains rules, max_size, 0, [["S"]]))
-      ([rules, max_size, index, generated_chains]
-            (if (= index (count generated_chains)) 
-                  generated_chains
-                  (let [
-                              chain_applied_changes
-                                    (GetApplyRulesInChain
-                                          rules     
-                                          (get generated_chains index)
-                                    )
-                              
-                              new_generated_chain (vec
-                                          (filter 
-                                                #(not (contains? (set generated_chains) %))
-                                                (filter 
-                                                      #(<= (count %) max_size)
-                                                      chain_applied_changes
-                                                )
-                                          )
-                                    )     
-                              
-                              new_possible_chain (vec (concat generated_chains new_generated_chain))
-                        ]      
-                  (GenerateAllPossibleChains rules, max_size, (+ index 1), new_possible_chain)
-                  )
-            )
-      )
-)
+  ([rules, max_size] (GenerateAllPossibleChains rules, max_size, 0, [["S"]]))
+  ([rules, max_size, index, generated_chains]
+   (if (= index (count generated_chains))
+     generated_chains
+     (let [chain_applied_changes
+           (GetApplyRulesInChain
+            rules
+            (get generated_chains index))
+
+           new_generated_chain (vec
+                                (filter
+                                 #(not (contains? (set generated_chains) %))
+                                 (filter
+                                  #(<= (count %) max_size)
+                                  chain_applied_changes)))
+
+           new_possible_chain (vec (concat generated_chains new_generated_chain))]
+       (GenerateAllPossibleChains rules, max_size, (+ index 1), new_possible_chain)))))
 
 (defn CheckIfChainIsAcceped [rules chain]
-      (let [
-                  max_size (count chain)
-                  all_possibilities (GenerateAllPossibleChains rules max_size)
-                  result (contains? (set all_possibilities) chain)
-            ]
-      result
-      )
-)
+  (let [max_size (count chain)
+        all_possibilities (GenerateAllPossibleChains rules max_size)
+        result (contains? (set all_possibilities) chain)]
+    result))
 
 ;; Main function ----------------------
 (defn -main []
-      (def gram_rules [
-                  [ "S" ["a", "A", "S"]  ]
-                  [ "S" ["a"]            ]
-                  [ "S" ["S", "S"]       ] 
-                  [ "A" ["b", "a"]       ]
-                  [ "A" ["S", "S"]       ]
-            ]
-      )
-      (println "Regras gramaticais: " gram_rules)
+  (def gram_rules [["S" ["a", "A", "S"]]
+                   ["S" ["a"]]
+                   ["S" ["S", "S"]]
+                   ["A" ["b", "a"]]
+                   ["A" ["S", "S"]]])
 
-      (def chain_to_be_recognized
-            ["b", "a"]
-      )
-      (println "Cadeia para ser reconhecida (w): " chain_to_be_recognized)
+  (println "Regras gramaticais: " gram_rules)
 
-      (def chain_is_acceped
-            (CheckIfChainIsAcceped gram_rules chain_to_be_recognized)
-      )
-      
-      (if chain_is_acceped
-            (println "Cadeia (w) foi Aceita :)")
-            (println "Cadeia (w) foi Rejeitada :(")
-      )
-)
+  (def chain_to_be_recognized
+    ["b", "a"])
+  (println "Cadeia para ser reconhecida (w): " chain_to_be_recognized)
+
+  (def chain_is_acceped
+    (CheckIfChainIsAcceped gram_rules chain_to_be_recognized))
+
+  (if chain_is_acceped
+    (println "Cadeia (w) foi Aceita :)")
+    (println "Cadeia (w) foi Rejeitada :(")))
--- ep2/test/ep2/core_test.clj
+++ ep2/test/ep2/core_test.clj
@@ -1,121 +1,79 @@
 (ns ep2.core-test
   (:require [clojure.test :refer :all]
-            [ep2.core :refer :all]
-    )
-)
+            [ep2.core :refer :all]))
 
 (deftest GetGramRuleTest []
-  (let [
-      rules_test [
-        [ "S" ["a", "A", "S"]  ]
-        [ "A" ["a"]            ]
-      ]
+  (let [rules_test [["S" ["a", "A", "S"]]
+                    ["A" ["a"]]]
 
-      value_result (ep2.core/GetGramRule rules_test 0)
+        value_result (ep2.core/GetGramRule rules_test 0)
 
-      value_result1 (ep2.core/GetGramRule rules_test 1)
-    ]
-  
+        value_result1 (ep2.core/GetGramRule rules_test 1)]
+
     (testing "Testando a função GetGramRule"
-      (is (= value_result {"simbol" "S", "value" ["a", "A", "S"] }))
-      (is (= (value_result "simbol") "S" ))
-      (is (= (value_result "value" ) ["a", "A", "S"] ))
-      (is (= value_result1 {"simbol" "A", "value" ["a"]} ))
-    )
-  )
-)
-
+      (is (= value_result {"simbol" "S", "value" ["a", "A", "S"]}))
+      (is (= (value_result "simbol") "S"))
+      (is (= (value_result "value") ["a", "A", "S"]))
+      (is (= value_result1 {"simbol" "A", "value" ["a"]})))))
 
 (deftest GetApplyRuleInElementTest []
-  (let [
-      rules_test0 [
-        [ "S" ["a", "A", "S"]  ]
-        [ "S" ["a"]            ]
-        [ "A" ["b" "a"]        ]
-      ]
+  (let [rules_test0 [["S" ["a", "A", "S"]]
+                     ["S" ["a"]]
+                     ["A" ["b" "a"]]]
 
-      value_result0 (ep2.core/GetApplyRuleInElement rules_test0 "S")
-      value_result1 (ep2.core/GetApplyRuleInElement rules_test0 "A")
-      value_result2 (ep2.core/GetApplyRuleInElement rules_test0 "a")
-    ]
-  
+        value_result0 (ep2.core/GetApplyRuleInElement rules_test0 "S")
+        value_result1 (ep2.core/GetApplyRuleInElement rules_test0 "A")
+        value_result2 (ep2.core/GetApplyRuleInElement rules_test0 "a")]
+
     (testing "Testando a função GetApplyRuleInElement"
-      (is (= value_result0 [["a", "A", "S"] ["a"]] ))
-      (is (= value_result1 [["b" "a"]] ))
-      (is (= value_result2 [] ))
-    )
-  )
-)
+      (is (= value_result0 [["a", "A", "S"] ["a"]]))
+      (is (= value_result1 [["b" "a"]]))
+      (is (= value_result2 [])))))
 
 (deftest GetApplyRulesInChainTest []
-  (let [
-      rules_test0 [
-        [ "S" ["a", "A", "S"]  ]
-        [ "S" ["a"]            ]
-        [ "A" ["b" "a"]        ]
-      ]
+  (let [rules_test0 [["S" ["a", "A", "S"]]
+                     ["S" ["a"]]
+                     ["A" ["b" "a"]]]
 
-      rules_test1 [
-        [ "S" ["a", "A", "S"]  ]
-        [ "S" ["a"]            ]
-      ]
+        rules_test1 [["S" ["a", "A", "S"]]
+                     ["S" ["a"]]]
 
-      value_result0 (ep2.core/GetApplyRulesInChain rules_test0 ["S", "A"])
-      value_result1 (ep2.core/GetApplyRulesInChain rules_test1 ["a", "A", "S"])
-    ]
-  
+        value_result0 (ep2.core/GetApplyRulesInChain rules_test0 ["S", "A"])
+        value_result1 (ep2.core/GetApplyRulesInChain rules_test1 ["a", "A", "S"])]
+
     (testing "Testando a função GetApplyRulesInChain"
-      (is (= value_result0 [["a", "A", "S", "A"], ["a", "A"], ["S", "b", "a"]]  ))
-      (is (= value_result1 [["a", "A", "a", "A", "S"], ["a", "A", "a"]]  ))
-    )
-  )
-)
+      (is (= value_result0 [["a", "A", "S", "A"], ["a", "A"], ["S", "b", "a"]]))
+      (is (= value_result1 [["a", "A", "a", "A", "S"], ["a", "A", "a"]])))))
 
 (deftest GenerateAllPossibleChainsTest []
-  (let [
-      rules_test0 [
-        [ "S" ["a"]            ]
-      ]
+  (let [rules_test0 [["S" ["a"]]]
 
-      rules_test1 [
-        [ "S" ["a", "A", "S"]  ]
-        [ "S" ["a"]            ]
-      ]
+        rules_test1 [["S" ["a", "A", "S"]]
+                     ["S" ["a"]]]
 
-      value_result0 (ep2.core/GenerateAllPossibleChains rules_test0 2)
-      value_result1 (ep2.core/GenerateAllPossibleChains rules_test1 3)
-    ]
-  
+        value_result0 (ep2.core/GenerateAllPossibleChains rules_test0 2)
+        value_result1 (ep2.core/GenerateAllPossibleChains rules_test1 3)]
+
     (testing "Testando a função GenerateAllPossibleChains"
-      (is (= value_result0 [["S"], ["a"]]  ))
-      (is (= value_result1 [["S"], ["a", "A", "S"], ["a"], ["a", "A", "a"]]  ))
-    )
-  )
-)
+      (is (= value_result0 [["S"], ["a"]]))
+      (is (= value_result1 [["S"], ["a", "A", "S"], ["a"], ["a", "A", "a"]])))))
 
 (deftest CheckIfChainIsAccepedTest []
-  (let [
-      rules_test0 [
-        [ "S" ["a", "A", "S"]  ]
-        [ "S" ["a"]            ]
-        [ "S" ["S", "S"]       ]
-        [ "A" ["b", "a"]       ]
-        [ "A" ["S", "S"]       ]
-      ]
+  (let [rules_test0 [["S" ["a", "A", "S"]]
+                     ["S" ["a"]]
+                     ["S" ["S", "S"]]
+                     ["A" ["b", "a"]]
+                     ["A" ["S", "S"]]]
 
-      chain_to_be_recognized0 ["a", "a"] 
-      chain_to_be_recognized1 ["a", "a","a","a"] 
-      chain_to_be_recognized2 ["b", "b"] 
+        chain_to_be_recognized0 ["a", "a"]
+        chain_to_be_recognized1 ["a", "a","a","a"]
+        chain_to_be_recognized2 ["b", "b"]
 
-      value_result0 (ep2.core/CheckIfChainIsAcceped rules_test0 chain_to_be_recognized0)
-      value_result1 (ep2.core/CheckIfChainIsAcceped rules_test0 chain_to_be_recognized1)
-      value_result2 (ep2.core/CheckIfChainIsAcceped rules_test0 chain_to_be_recognized2)
-    ]
-  
+        value_result0 (ep2.core/CheckIfChainIsAcceped rules_test0 chain_to_be_recognized0)
+        value_result1 (ep2.core/CheckIfChainIsAcceped rules_test0 chain_to_be_recognized1)
+        value_result2 (ep2.core/CheckIfChainIsAcceped rules_test0 chain_to_be_recognized2)]
+
     (testing "Testando a função CheckIfChainIsAcceped"
       (is value_result0)
       (is value_result1)
-      (is (not value_result2))
-    )
-  )
-)
+      (is (not value_result2)))))
