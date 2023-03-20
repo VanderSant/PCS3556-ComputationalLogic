9d58827fe201129b5f2763fbce7701c602ceffaa
VanderSant <vanderson.santos@enline-transmission.com>
2023-03-20 01:54:59 -0300

    ♻️ Refact ep1 core function

--- ep1/src/ep1/core.clj
+++ ep1/src/ep1/core.clj
@@ -5,13 +5,18 @@
 
 ;; Basix functions ----------------------------------------------------------------------------
 (defn CreateR [i_array j_array]
-  (let [ result (map vector i_array j_array) ]
-  (vec result))
+  (let [
+      result (map vector i_array j_array) 
+    ]
+  (vec result)
+  )
 )
 
 (defn CreateReflectiveR[R PossibleReflectiveValues]
-  (def DataRemoveCondition (fn [x] (contains? (set R) x)) )
-  (let [return (remove DataRemoveCondition PossibleReflectiveValues)]
+  (let [
+      DataRemoveCondition (fn [x] (contains? (set R) x)) 
+      return (remove DataRemoveCondition PossibleReflectiveValues)
+    ]
   (clojure.set/union R return)
   )
 )
@@ -19,22 +24,18 @@
 ;; Transitive function using recursion -------------------------------------------------------
 
 (defn CreateBinaryMatrix [n coords]
-  (let [matrix (vec (repeat n (vec (repeat n 0))))] ; cria matriz vazia
-    (do 
-      (def result matrix)
-      (doseq [[x y] coords]
-        (do
-          (def result (assoc-in result [(- x 1) (- y 1)] 1))
-        )
-      )
-    )
-    result
+  (let [
+      line_indexes (range n)
+      column_indexes (range n)
+      GenIndexValue (fn [i j] (if (contains? (set coords) [(+ i 1) (+ j 1)]) 1 0))
+      GenMatrixLine (fn [i] (vec (map (partial GenIndexValue i) column_indexes) ) )
+      GenAllMatrix (map GenMatrixLine line_indexes)
+    ]
+    GenAllMatrix
   )
 )
 
-(defn MatrixMult
-  [mat1 mat2]
-
+(defn MatrixMult [mat1 mat2]
   (let [ 
       Transpose #(apply map vector %)
       DotProduct #(reduce + (map * %1 %2)) ;; product all values and sum (produto escalar)
@@ -62,28 +63,15 @@
 )
 
 (defn GetBinatyMatrixIndex[n matrix]
-    (do
-      (def result [])
-      (def i 0)
-      (def j 0)
-      (while (< i n)
-        (do
-          (while (< j n)
-            (do
-              (if (= 1 (get-in matrix [i j]))
-                (do
-                  (def result (conj result [(+ i 1) (+ j 1)]))
-                )
-              )
-              (def j (+ j 1))
-            )
-          )
-          (def j 0)
-          (def i (+ i 1))
-        )
-      )
-  )
-    result
+    (let [
+      line_indexes (range n)
+      column_indexes (range n)
+      GetMatrixIndex (fn [i j] (if (= 1 (get-in matrix [i j])) [(+ i 1) (+ j 1)] [] ) )
+      SearchAllLine (fn [i] (vec (map (partial GetMatrixIndex i) column_indexes) ) )
+      AllIndexes (remove empty? (apply concat (map SearchAllLine line_indexes) ) )
+    ]
+    AllIndexes
+    )
 )
 
 (defn MatrixOr [matrix_a matrix_b]
@@ -98,18 +86,24 @@
 )
 
 (defn GetTransitiveR [n R Rn]
-  (def Rn_plus1 (TranformMatrixInBinaryMatrix (MatrixMult R Rn)) )
-  (if (= Rn_plus1 Rn)
-    Rn
-    (MatrixOr Rn (GetTransitiveR n R Rn_plus1) )
+  (let [ 
+      Rn_plus1 (TranformMatrixInBinaryMatrix (MatrixMult R Rn))
+    ]
+    (if (= Rn_plus1 Rn)
+      Rn
+      (MatrixOr Rn (GetTransitiveR n R Rn_plus1) )
+    )
   )
 )
 
 (defn Transitive [n R]
-  (def R_binary (CreateBinaryMatrix n R))
-  (def R_final_binary (GetTransitiveR n R_binary R_binary) )
-  (def R_final (GetBinatyMatrixIndex n R_final_binary) )
+  (let [
+    R_binary (CreateBinaryMatrix n R)
+    R_final_binary (GetTransitiveR n R_binary R_binary)
+    R_final (GetBinatyMatrixIndex n R_final_binary)
+  ]
   R_final
+  )
 )
 
 ;; Main function -----------------------------------------------------------------------------
@@ -140,4 +134,3 @@
   )
   (println "Fecho Transitivo de R | Transitive(R):" TransitiveR)
 )
-
