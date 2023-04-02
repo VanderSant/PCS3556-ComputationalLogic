4da43bbe091a2149331ab45c974b53c90c561a35
VanderSant <vanderson.santos@enline-transmission.com>
2023-04-02 18:49:07 -0300

    🔨 add ep2 to ep4

--- ep4/project.clj
+++ ep4/project.clj
@@ -2,9 +2,14 @@
   :main ep4.core
   :dependencies [
     [org.clojure/clojure "1.5.1"]
+    [PCS3556/ep2 "0.1.0-SNAPSHOT"]
   ]
 
-  :plugins [[lein-cljfmt "0.9.2"]]
+  :plugins [[lein-cljfmt "0.9.2"] 
+            [lein-auto-install "0.1.1"]]
   
   :test-paths ["test/"]
+  ;; :pedantic? false
+  ;; :warn-on-reflection false
+  ;; :lein-auto-install {:pedantic? false :warn-on-reflection false}
 )
