6678d1b785aca428a5cc2379135bdd833675999a
VanderSant <vanderson.santos@enline-transmission.com>
2023-04-17 04:24:11 -0300

    🔨 add some ep4 functions

--- ep4/src/ep4/core.clj
+++ ep4/src/ep4/core.clj
@@ -6,25 +6,71 @@
 
 ;; Finite Automaton Functions --------------------
 
+(defn GetAllPossibleSymbols [grammar]
+  (let [
+    GetSymbol #(nth % 0)
+    get_all_symbols (vec (map GetSymbol grammar))
+    all_symbols_without_duplicate (distinct get_all_symbols)
+  ]
+    all_symbols_without_duplicate
+  )
+)
 
-;; Main functions --------------------
+(defn IsRuleWithTwoVariable
+  [grammar start_symbol end_symbols index]
+  (let [
+    rule (ep2/GetGramRule grammar index)
+    symbol (rule "simbol")
+    values (rule "value")
+
+    all_symbols (GetAllPossibleSymbols grammar)
+    number_of_symbols_in_values (count (filter #(contains? (set all_symbols) %) values))
+    is_the_right_number_of_variable (= number_of_symbols_in_values 2)
+  ]
+    is_the_right_number_of_variable
+  )
+)
+
+(defn IsRuleWithOneTerminal
+  [grammar start_symbol end_symbols index]
+  (let [
+    rule (ep2/GetGramRule grammar index)
+    symbol (rule "simbol")
+    values (rule "value")
+
+    number_of_termminals_in_values (count (filter #(contains? (set end_symbols) %) values))
+    is_the_right_number_of_terminal (= number_of_termminals_in_values 1)
+  ]
+    is_the_right_number_of_terminal
+  )
+)
+
+(defn IsThatGrammerInChomskyNormalForm 
+  [grammar start_symbol end_symbols]
+
+)
 
 
 ;; Main function ----------------------
 
 (defn -main []
-  (def gram_rules [["S" ["a", "A", "S"]]
-                   ["S" ["a"]]
-                   ["S" ["S", "S"]]
-                   ["A" ["b", "a"]]
-                   ["A" ["S", "S"]]])
+  (def grammar [["S" ["A", "S", "A"]]
+                   ["S" ["a", "B"]]
+                   ["A" ["B"]]
+                   ["A" ["S"]]
+                   ["B" ["b", "ε"]]])
+  
+  (def start_symbol "S")
+  (def end_symbols ["a" "b"])
 
-  (println "Regras gramaticais: " gram_rules)
+  (println "Regras gramaticais: " grammar)
+  (println "Simbolos iniciais: " start_symbol)
+  (println "Simbolos finais: " end_symbols)
 
   (def chain_to_be_recognized
     ["b", "a"])
   (println "Cadeia para ser reconhecida (w): " chain_to_be_recognized)
 
   (def chain_is_acceped
-    (ep2/CheckIfChainIsAcceped gram_rules chain_to_be_recognized))
+    (ep2/CheckIfChainIsAcceped grammar chain_to_be_recognized))
 )
--- ep4/test/ep4/core_test.clj
+++ ep4/test/ep4/core_test.clj
@@ -3,3 +3,80 @@
             [ep4.core :refer :all]))
 
 ;; functions tests --------------------
+(deftest GetAllPossibleSymbolsTest []
+  (let
+    [
+      grammar [["S" ["A", "S", "A"]]
+               ["S" ["a", "B"]]
+               ["A" ["B"]]
+               ["A" ["S"]]
+               ["B" ["b", "ε"]]]
+                   
+      result (ep4.core/GetAllPossibleSymbols grammar)
+    ]
+
+    (testing "Testando a função GetAllPossibleSymbols"
+      (is (= result ["S" "A" "B" ]))
+    )
+  )
+)
+
+(deftest IsRuleWithTwoVariableTest []
+  (let
+    [
+      grammar [["S" ["A", "S", "A"]]
+               ["S" ["a", "B"]]
+               ["A" ["B" "A"]]
+               ["A" ["S"]]
+               ["B" ["b", "ε"]]]
+      
+      start_symbol "S"
+
+      end_symbols ["a" "b"]
+                   
+      result0 (ep4.core/IsRuleWithTwoVariable grammar start_symbol end_symbols 0)
+      result1 (ep4.core/IsRuleWithTwoVariable grammar start_symbol end_symbols 1)
+      result2 (ep4.core/IsRuleWithTwoVariable grammar start_symbol end_symbols 2)
+      result3 (ep4.core/IsRuleWithTwoVariable grammar start_symbol end_symbols 3)
+      result4 (ep4.core/IsRuleWithTwoVariable grammar start_symbol end_symbols 4)
+    ]
+
+    (testing "Testando a função IsRuleWithTwoVariable"
+      (is (not result0) )
+      (is (not result1) )
+      (is result2 )
+      (is (not result3) )
+      (is (not result4) )
+    )
+  )
+)
+
+(deftest IsRuleWithOneTerminalTest []
+  (let
+    [
+      grammar [["S" ["A", "S", "A"]]
+               ["S" ["a", "B", "a"]]
+               ["A" ["B" "A"]]
+               ["A" ["S"]]
+               ["B" ["b", "ε"]]]
+      
+      start_symbol "S"
+
+      end_symbols ["a" "b"]
+                   
+      result0 (ep4.core/IsRuleWithOneTerminal grammar start_symbol end_symbols 0)
+      result1 (ep4.core/IsRuleWithOneTerminal grammar start_symbol end_symbols 1)
+      result2 (ep4.core/IsRuleWithOneTerminal grammar start_symbol end_symbols 2)
+      result3 (ep4.core/IsRuleWithOneTerminal grammar start_symbol end_symbols 3)
+      result4 (ep4.core/IsRuleWithOneTerminal grammar start_symbol end_symbols 4)
+    ]
+
+    (testing "Testando a função IsRuleWithTwoVariable"
+      (is (not result0) )
+      (is (not result1) )
+      (is (not result2) )
+      (is (not result3) )
+      (is result4 )
+    )
+  )
+)
