e4d613b8083aa401fcca564e5ee84f7020352c32
VanderSant <vanderson.santos@enline-transmission.com>
2023-08-16 18:49:18 -0300

    🔧 update ep4 and add class 7 exercise

--- ep4/project.clj
+++ ep4/project.clj
@@ -2,14 +2,10 @@
   :main ep4.core
   :dependencies [
     [org.clojure/clojure "1.5.1"]
-    [PCS3556/ep2 "0.1.0-SNAPSHOT"]
+    [PCS3556/ep2 "0.1.0-SNAPSHOT"  ]
   ]
 
-  :plugins [[lein-cljfmt "0.9.2"] 
-            [lein-auto-install "0.1.1"]]
+  :plugins [[lein-cljfmt "0.9.2"]]
   
   :test-paths ["test/"]
-  ;; :pedantic? false
-  ;; :warn-on-reflection false
-  ;; :lein-auto-install {:pedantic? false :warn-on-reflection false}
 )
